6c88a64f968822dea3062c62d01f2430286318b6
Kyan <weiliang.chia@gmail.com>
2023-03-12 23:00:26 +0800

    created deck feature on frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,17 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-
+import cors from "cors";
 import Deck from "./models/Deck";
 
 const PORT = 5200;
 
 const app = express();
 
+app.use(cors({
+        origin: "*",
+    })
+);
 app.use(express.json());
 
 app.get('/', (req: Request , res: Response) => {
